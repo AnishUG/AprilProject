7c504a4ad46a33ad2d35d1b7bade28601369ff6a
anish <anish@DESKTOP-JETEBKU>
2022-04-19 20:51:47 +0530

    Added some new classes

---
+++ MavenEx/src/main/java/com/pages/RegistrationPage.java
@@ -0,0 +1,45 @@
+package com.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class RegistrationPage {
+	
+	WebDriver driver;
+	
+	@FindBy (id="name")
+	public WebElement name;
+	
+	@FindBy (id="mobile")
+	public WebElement mobile;
+	
+	@FindBy (id="email")
+	public WebElement email;
+	
+	@FindBy (id="password")
+	public WebElement password;
+	
+	@FindBy (xpath="//button")
+	public WebElement button;
+	
+	
+	public RegistrationPage(WebDriver driver) {
+		
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+
+
+	public void creatinguser () {
+		
+		name.sendKeys("anjh");
+		mobile.sendKeys("87767677");
+		email.sendKeys("ahjhj@Test.com");
+		password.sendKeys("87656767");
+		button.click();
+		
+	}
+
+}
---
+++ MavenEx/src/test/java/com/jbk/RegistrationTest.java
@@ -0,0 +1,35 @@
+package com.jbk;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.pages.RegistrationPage;
+
+public class RegistrationTest {
+	
+	WebDriver driver;
+	
+	@Test
+	
+	public void CreateUser () {
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		
+		driver.get("file:///E:/Automation%20testing/Class%20video%20link/Offline%20website/javabykiran-Selenium-Softwares/javabykiran-Selenium-Softwares/Offline%20Website/pages/examples/register.html");
+		
+		RegistrationPage RR = new RegistrationPage (driver);
+		
+		RR.creatinguser();
+		
+//	Alert alert =	driver.switchTo().alert();
+//	alert.accept();
+		
+		
+	}
+
+}
